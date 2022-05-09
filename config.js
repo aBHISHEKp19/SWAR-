12186e3130ff285d11ff2c9ffa4c84199042131e
aBHISHEKp <abhishekpoddar4776@gmail.com>
2022-05-09 20:46:37 +0530

    E

---
+++ config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    app: {
+        px: 'S.',
+        token: 'OTcyMTY2NTkxNDczMTU2MDk2.G5mTlp.WOrtVCqaAPs6a3QKHaooUIISp_Lv_bGZDno44s',
+       // playing: 'by Zerio ❤️'
+    },
+
+    opt: {
+        DJ: {
+            enabled: false,
+            roleName: 'DJ',
+            commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'seek', 'shuffle', 'skip', 'stop', 'volume']
+        },
+        maxVol: 100,
+        loopMessage: false,
+        discordPlayer: {
+            ytdlOptions: {
+                quality: 'highestaudio',
+                highWaterMark: 1 << 25
+            }
+        }
+    }
+};
