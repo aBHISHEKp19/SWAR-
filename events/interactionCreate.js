540b2b6b43f91cab1ac36d87d8ecb47e17a0e235
aBHISHEKp <abhishekpoddar4776@gmail.com>
2022-06-13 18:06:16 +0530

    vsds

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -8,9 +8,9 @@ module.exports = (client, int) => {
             if (!queue || !queue.playing) return int.reply({ content: `No music currently playing... try again ? ❌`, ephemeral: true, components: [] });
 
             int.member.send(`You saved the track ${queue.current.title} | ${queue.current.author} from the server ${int.member.guild.name} ✅`).then(() => {
-                return int.reply({ content: `I have sent you the title of the music by private messages ✅`, ephemeral: true, components: [] });
+                return int.reply({ content: `I have sent you the title of the music in dm ✅`, ephemeral: true, components: [] });
             }).catch(error => {
-                return int.reply({ content: `Unable to send you a private message... try again ? ❌`, ephemeral: true, components: [] });
+                return int.reply({ content: `Unable to send you a private message your dms are off`, ephemeral: true, components: [] });
             });
         }
     }
