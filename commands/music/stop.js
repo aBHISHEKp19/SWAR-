f62b3f14b35c95da3f6923fa303c708aae120bb5
aBHISHEKp <abhishekpoddar4776@gmail.com>
2022-06-17 20:02:32 +0530

    dvsvds

--- commands/music/stop.js
+++ commands/music/stop.js
@@ -7,7 +7,7 @@ module.exports = {
     execute(client, message) {
         const queue = player.getQueue(message.guild.id);
 
-        if (!queue || !queue.playing) return message.channel.send(`No music currently playing ${message.author}... try again ? ❌`);
+       // if (!queue || !queue.playing) return message.channel.send(`No music currently playing ${message.author}... try again ? ❌`);
 
         queue.destroy();
 
