7701aa73c8fc996a2ffda4e043b5716c92430843
aBHISHEKp <abhishekpoddar4776@gmail.com>
2022-06-17 20:00:52 +0530

    xcca

--- commands/core/help.js
+++ commands/core/help.js
@@ -14,11 +14,11 @@ module.exports = {
 
         const commands = client.commands.filter(x => x.showHelp !== false);
 
-       // embed.setDescription('This code comes from a GitHub project [ZerioDev/Music-bot](https://github.com/ZerioDev/Music-bot).\nThe use of this one is possible while keeping the credits for free.\nIf you want to remove the credits join the Discord support server.');
+       
         embed.addField(`Enabled - ${commands.size}`, commands.map(x => `\`${x.name}${x.aliases[0] ? ` (${x.aliases.map(y => y).join(', ')})\`` : '\`'}`).join(' | '));
 
         embed.setTimestamp();
-        embed.setFooter('Music comes first - Made with heart by appy', message.author.avatarURL({ dynamic: true }));
+        embed.setFooter('Music comes first - Made by appy', message.author.avatarURL({ dynamic: true }));
 
         message.channel.send({ embeds: [embed] });
     },
