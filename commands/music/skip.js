4ec36da2a59ba3349b6a3dca14b3e359f6fead4b
aBHISHEKp <abhishekpoddar4776@gmail.com>
2022-06-21 21:52:12 +0530

    skip

--- commands/music/skip.js
+++ commands/music/skip.js
@@ -1,3 +1,6 @@
+
+const { MessageEmbed } = require('discord.js')
+
 module.exports = {
     name: 'skip',
     aliases: ['sk'],
@@ -5,12 +8,16 @@ module.exports = {
     voiceChannel: true,
 
     execute(client, message) {
+
+        const HJDDJ = new MessageEmbed()
+            .setColor('RANDOM')
+            .setDescription(success ? `Current music ${queue.current.title} skipped <a:swartik:988048652901699584>` : `Something went wrong ${message.author}... try again ? ❌`);
         const queue = player.getQueue(message.guild.id);
 
         if (!queue || !queue.playing) return message.channel.send(`No music currently playing ${message.author}... try again ? ❌`);
 
         const success = queue.skip();
 
-        return message.channel.send(success ? `Current music ${queue.current.title} skipped ✅` : `Something went wrong ${message.author}... try again ? ❌`);
+        return message.channel.send({ embeds: [HJDDJ] });
     },
 };
