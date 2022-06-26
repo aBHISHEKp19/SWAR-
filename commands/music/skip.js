252b5da772e28c059015ad960ad3ab4ecf04af20
aBHISHEKp <abhishekpoddar4776@gmail.com>
2022-06-26 12:14:02 +0530

    sk

--- commands/music/skip.js
+++ commands/music/skip.js
@@ -8,15 +8,14 @@ module.exports = {
     voiceChannel: true,
 
     execute(client, message) {
-
-        const HJDDJ = new MessageEmbed()
-            .setColor('RANDOM')
-            .setDescription(success ? `Current music ${queue.current.title} skipped <a:swartik:988048652901699584>` : `Something went wrong ${message.author}... try again ? ❌`);
         const queue = player.getQueue(message.guild.id);
-
         if (!queue || !queue.playing) return message.channel.send(`No music currently playing ${message.author}... try again ? ❌`);
 
         const success = queue.skip();
+        
+        const HJDDJ = new MessageEmbed()
+            .setColor('RANDOM')
+            .setDescription(success ? `Current music ${queue.current.title} skipped <a:swartik:988048652901699584>` : `Something went wrong ${message.author}... try again ? ❌`);
 
         return message.channel.send({ embeds: [HJDDJ] });
     },
