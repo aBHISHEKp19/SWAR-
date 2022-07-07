1e6c3abe9bf7ac413c0aacda9b90d20aa17feb35
aBHISHEKp <abhishekpoddar4776@gmail.com>
2022-07-07 22:58:10 +0530

    c

--- commands/music/stop.js
+++ commands/music/stop.js
@@ -5,8 +5,8 @@ module.exports = {
     voiceChannel: true,
 
     execute(client, message) {
-        /* const queue = player.getQueue(message.guild.id);
-         const ddv = new MessageEmbed()
+         const queue = player.getQueue(message.guild.id);
+         /*const ddv = new MessageEmbed()
          .setColor('#da004e')*
          .setDescription(`Music stopped into this server, see you next time <a:swartik:988048652901699584>`);*/
         if (!queue || !queue.playing) return message.channel.send(`No music currently playing ${message.author}... try again ? ❌`);
