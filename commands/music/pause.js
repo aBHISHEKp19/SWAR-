3533c00bd178184904743ec6e5d52b529b614955
aBHISHEKp <abhishekpoddar4776@gmail.com>
2022-07-20 21:47:06 +0530

    dd

--- commands/music/pause.js
+++ commands/music/pause.js
@@ -9,13 +9,13 @@ module.exports = {
     execute(client, message) {
         const queue = player.getQueue(message.guild.id);
 
-        const llol = new Discord.Messagembed()
+       /* const llol = new Discord.Messagembed()
         .setDescription('')
-
+*/
         if (!queue) return message.channel.send(`No music currently playing ${message.author}... try again ? <a:No_1:999332118368891010>`);
 
         const success = queue.setPaused(true);
 
         return message.channel.send(success ? `Current music ${queue.current.title} paused ✅` : `Something went wrong ${message.author}... try again ? <a:No_1:999332118368891010>`);
-    },
+    }
 };
--- commands/music/stop.js
+++ commands/music/stop.js
@@ -5,14 +5,14 @@ module.exports = {
     voiceChannel: true,
 
     execute(client, message) {
-         const queue = player.getQueue(message.guild.id);
-         /*const ddv = new MessageEmbed()
-         .setColor('#da004e')*
-         .setDescription(`Music stopped into this server, see you next time <a:swartik:988048652901699584>`);*/
+        const queue = player.getQueue(message.guild.id);
+        const ddv = new MessageEmbed()
+            .setColor('#da004e')
+            .setDescription(`Music stopped into this server, see you next time <a:swartik:988048652901699584>`);
         if (!queue || !queue.playing) return message.channel.send(`No music currently playing ${message.author}... try again ?<a:No_1:999332118368891010>`);
 
         queue.destroy();
 
-        message.channel.send(`Music stopped into this server, see you next time <a:swartik:988048652901699584>`);
+        message.channel.send({ embeds: [ddv] });
     },
 };
