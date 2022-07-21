14106262cf801834d4d201de90f5b96209520181
aBHISHEKp <abhishekpoddar4776@gmail.com>
2022-07-21 11:27:10 +0530

    s

--- commands/music/pause.js
+++ commands/music/pause.js
@@ -1,5 +1,5 @@
 const { DiscordAPIError } = require("discord.js");
-
+const { MessageEmbed } = require('discord.js')
 module.exports = {
     name: 'pause',
     aliases: [],
@@ -9,13 +9,14 @@ module.exports = {
     execute(client, message) {
         const queue = player.getQueue(message.guild.id);
 
-       /* const llol = new Discord.Messagembed()
-        .setDescription('')
-*/
+
+
         if (!queue) return message.channel.send(`No music currently playing ${message.author}... try again ? <a:No_1:999332118368891010>`);
 
         const success = queue.setPaused(true);
-
-        return message.channel.send(success ? `Current music ${queue.current.title} paused ✅` : `Something went wrong ${message.author}... try again ? <a:No_1:999332118368891010>`);
+        const dgrgdv = new MessageEmbed()
+            .setColor('#da004e')
+            .setDescription(success ? `Current music ${queue.current.title} paused<a:swartik:988048652901699584>` : `Something went wrong ${message.author}... try again ? <a:No_1:999332118368891010>`);
+        return message.channel.send({embeds:  [dgrgdv]})
     }
 };
