5388e70cce29c06ff412bb0d22905cec2f4afcce
aBHISHEKp <abhishekpoddar4776@gmail.com>
2022-09-11 22:13:06 +0530

    eihfi

--- commands/music/seek.js
+++ commands/music/seek.js
@@ -7,16 +7,22 @@ module.exports = {
     voiceChannel: true,
 
     async execute(client, message, args) {
-        const queue = player.getQueue(message.guild.id);
 
-        if (!queue || !queue.playing) return message.channel.send(`No music currently playing ${message.author}... try again ? <a:No_1:999332118368891010>`);
+        if (message.content == ">>seek") {
+            message.channel.send(`**This command is under maintenance**${message.author}`);
+        }
 
-        const timeToMS = ms(args.join(' '));
+        /*
+     const queue = player.getQueue(message.guild.id);
 
-        if (timeToMS >= queue.current.durationMS) return message.channel.send(`The indicated time is higher than the total time of the current song ${message.author}... try again ? <a:No_1:999332118368891010>\n*Try for example a valid time like **5s, 10s, 20 seconds, 1m**...*`);
+     if (!queue || !queue.playing) return message.channel.send(`No music currently playing ${message.author}... try again ? <a:No_1:999332118368891010>`);
 
-        await queue.seek(timeToMS);
+     const timeToMS = ms(args.join(' '));
 
-        message.channel.send(`Time set on the current song **${ms(timeToMS, { long: true })}** ✅`);
+     if (timeToMS >= queue.current.durationMS) return message.channel.send(`The indicated time is higher than the total time of the current song ${message.author}... try again ? <a:No_1:999332118368891010>\n*Try for example a valid time like **5s, 10s, 20 seconds, 1m**...*`);
+
+     await queue.seek(timeToMS);
+
+     message.channel.send(`Time set on the current song **${ms(timeToMS, { long: true })}** ✅`);*/
     },
 };
--- commands/music/volume.js
+++ commands/music/volume.js
@@ -1,5 +1,5 @@
 const maxVol = client.config.opt.maxVol;
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed, DiscordAPIError } = require('discord.js');
 module.exports = {
     name: 'volume',
     aliases: ['vol'],
@@ -7,24 +7,32 @@ module.exports = {
     voiceChannel: true,
 
     execute(client, message, args) {
-   
-     
+        const voll = new MessageEmbed()
+            .setColor('RANDOM')
+            .setDescription(`The volume you wanna set is already the current volume dumbo ${message.author} <a:No_1:999332118368891010>`);
+        const hfjs = new MessageEmbed()
+            .setColor('RANDOM')
+            .setDescription(`Enter a valid number between 1-100 ${message.author}<a:No_1:999332118368891010>`);
+        const ksnck = new MessageEmbed()
+            .setColor('RANDOM')
+            .setDescription(`No music currently playing ${message.author}... try again ? <a:No_1:999332118368891010>`);
+
         const queue = player.getQueue(message.guild.id);
 
-        if (!queue || !queue.playing) return message.channel.send(`No music currently playing ${message.author}... try again ? <a:No_1:999332118368891010>`);
+        if (!queue || !queue.playing) return message.channel.send({ embeds: [ksnck] });
 
         const vol = parseInt(args[0]);
 
         if (!vol) return message.channel.send(`The current volume is ${queue.volume} 🔊\n*To change the volume enter a valid number between **1** and **${maxVol}**.*`);
 
-        if (queue.volume === vol) return message.channel.send(`The volume you want to change is already the current one ${message.author}... try again ? <a:No_1:999332118368891010>`);
+        if (queue.volume === vol) return message.channel.send({ embeds: [voll] });
 
-        if (vol < 0 || vol > maxVol) return message.channel.send(`The specified number is not valid. Enter a number between **1** and **${maxVol}** ${message.author}... try again ? <a:No_1:999332118368891010>`);
+        if (vol < 0 || vol > maxVol) return message.channel.send({ embeds: [hfjs] });
 
         const success = queue.setVolume(vol);
         const sds = new MessageEmbed()
-        .setColor('#da004e')
-        .setDescription(success ? `The volume has been modified to **${vol}**/**${maxVol}**% <a:str_music:999330140859404388>` : `Something went wrong ${message.author}... try again ? <a:No_1:999332118368891010>`);
+            .setColor('#da004e')
+            .setDescription(success ? `The volume has been modified to **${vol}**/**${maxVol}**% <a:str_music:999330140859404388>` : `Something went wrong ${message.author}... try again ? <a:No_1:999332118368891010>`);
 
         return message.channel.send({ embeds: [sds] });
     },
